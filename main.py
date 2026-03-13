756cd6a42f32eee0455a5ce0dea3cf1731a09e84
Aniket-Dutta28 <aniketdutta282003@gmail.com>
2026-03-13 19:39:51 +0530

    all task fetching fuction added

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
+from typing import Annotated
 from database import Engine
 import models
+from pydantic import BaseModel,Field
+from database import session_local
+from sqlalchemy.orm import Session
+from models import todo
+
 
 
 models.Base.metadata.create_all(bind=Engine)
 app = FastAPI()
 
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close
+
+db_dependency = Annotated[Session,Depends(get_db)]
+
 @app.get("/")
-async def test_connection():
-    return {"message":"Testing Connection"}
+async def get_task(db:db_dependency):
+    return db.query(todo).all()
